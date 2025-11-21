72fcecaebed08badb705d71247b2f5bbf2bf1a8c
vaasacare-health <vaasa.care@gmail.com>
2025-11-21 14:49:34 +0530

    Update vaasa_bot.py

--- vaasa_bot.py
+++ vaasa_bot.py
@@ -3,7 +3,7 @@ import telebot
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN not set in environment")
+    raise SystemExit("BOT_TOKEN env var is missing")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
